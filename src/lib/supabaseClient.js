2e0c8a6f6346290e5a19f099db31aa1b5b04fd44
Muhammad Haris Nadeem <bscs21062@itu.edu.pk>
2025-05-29 02:08:50 +0500

    auth changes

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,4 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://fpnifsrtizyodnoryrml.supabase.co'; // Replace with your URL
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZwbmlmc3J0aXp5b2Rub3J5cm1sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0NTYzODEsImV4cCI6MjA2NDAzMjM4MX0.JzTzeoEJWED387EqLVkvxq6jocfJeXpJ4EK9lgT0Oxg'; // Replace with your anon key
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
