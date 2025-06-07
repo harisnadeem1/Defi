755dafcce7fa42880d84ada2422b6ddbd4b768af
Muhammad Haris Nadeem <bscs21062@itu.edu.pk>
2025-06-08 04:15:00 +0500

    importing img avat

--- src/components/ReviewCarousel.jsx
+++ src/components/ReviewCarousel.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Star } from "lucide-react";
 import { motion } from "framer-motion";
+import liam from "../assets/Avatars/Liam-Parker.webp"
 
 const reviews =[
   {
     name: "Liam Parker",
-    image: "/Avatars/Liam-Parker.webp",
+    image: liam,
     message: "DeFiStrategies helped me understand how to earn passive income through staking. The tutorials are super clear and beginner-friendly. I started earning yield within days!"
   },
   {
